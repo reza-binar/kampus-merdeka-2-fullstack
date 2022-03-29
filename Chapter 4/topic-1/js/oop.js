01a861d691efaac75036e3f3533a1210c007b950
Fahmi Alfareza <fahmialfareza@icloud.com>
2022-03-29 21:36:12 +0700

    Abstraction, Encapsulation, Polymorphism

--- Chapter 4/topic-1/js/oop.js
+++ Chapter 4/topic-1/js/oop.js
@@ -6,11 +6,19 @@ class Human {
   constructor(name, address) {
     this.name = name;
     this.address = address;
+    if (this.constructor === Human) {
+      throw new Error('Human is an abstract class');
+    }
   }
 
   // Instance method
   introduce() {
     console.log(`Hi, I'm ${this.name}`);
+  }
+
+  // protected method
+  _relaxing() {
+    console.log(`${this.name} is relaxing`);
   }
 }
 
@@ -38,19 +46,27 @@ class Programmer extends Human {
   //   console.log(`I can write`, this.programmingLanguages);
   // }
 
-  // Overload method
+  // Overload method (public)
   introduce(name) {
+    super._relaxing(); // Call protected method
     super.introduce();
     console.log(`Hi ${name}, I'm ${this.name} and I'm a programmer`);
   }
 
+  // public
   code() {
+    this.#doGossip();
     console.log(
       `Code some`,
       this.programmingLanguages[
         Math.floor(Math.random() * this.programmingLanguages.length)
       ]
     );
+  }
+
+  // private, it can not be accessed outside class
+  #doGossip() {
+    console.log(`My address will become viral ${this.address}`);
   }
 }
 
@@ -64,3 +80,5 @@ console.log(fajar instanceof Human); // Output: true
 fajar.introduce(`Vito`); // Output: Hi, I'm Fajar
 fajar.greet('Arif'); // Output: Hi Arif, I'm Fajar
 Programmer.destroy('Laptop'); // Output: Programmer is destroying Laptop
+
+fajar.code(); // Output: My address will become viral Madiun
